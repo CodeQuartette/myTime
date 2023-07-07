b101c740e5eea7e928efc3c72cdeb46d3021201b
jeong-inho <derosatam76@gmail.com>
2023-07-07 17:00:37 +0900

    feat[#1]: .gitignore 파일 생성

---
+++ src/test/java/com/beautifulGilan/myTime/MyTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautifulGilan.myTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

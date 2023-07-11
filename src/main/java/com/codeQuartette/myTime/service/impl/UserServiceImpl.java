4e8489824df64c35fe1babb6a0cace6da904f8b4
Tree <gmldbs1109@naver.com>
2023-07-11 15:31:11 +0900

    feat[#5]: 데이터 연동 확인을 위한 테스트 API 작성
    - 추가된 데이터를 조회할 수 있는지 확인하는 API 작성

--- src/main/java/com/codeQuartette/myTime/service/impl/UserServiceImpl.java
+++ src/main/java/com/codeQuartette/myTime/service/impl/UserServiceImpl.java
@@ -1,13 +1,25 @@
 package com.codeQuartette.myTime.service.impl;
 
+import com.codeQuartette.myTime.domain.User;
 import com.codeQuartette.myTime.repository.UserRepository;
 import com.codeQuartette.myTime.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    @Override
+    public void testUser() {
+        User tree = userRepository.findById(1L).orElseThrow(
+                () -> new NoSuchElementException("트리 없다.")
+        );
+
+        System.out.println("tree.getNickname() = " + tree.getNickname());
+    }
 }

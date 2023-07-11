3cde6ec84e9398a54cc1b2618e7d7d8e5b36878b
Tree <gmldbs1109@naver.com>
2023-07-11 15:28:36 +0900

    feat[#5]: test를 위한 데이터 추가 및 설정
    - data.sql 에 데이터 추가
    - application.yml에 data가 추가될 수 있도록 설정 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user`(name, nickname, email, token, birthday, password, profile_image, gender)
+VALUES ('최희윤', '트리', 'tree@gmail.com', 'token', '1991-11-09', 'password', '/image/tree.jpg', 1);

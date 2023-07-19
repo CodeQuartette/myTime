e021031bc79ef1d96bfb579965a390f3a384a108
Tree <gmldbs1109@naver.com>
2023-07-20 00:08:14 +0900

    feat[#10]: 습관 생성 API 구현
    - MyDate에 date와 user만 같다면 같은 값으로 판단하도록
    equals, hashCode 메소드 override
    - 조인 테이블의 List 추가

--- src/main/java/com/codeQuartette/myTime/domain/MyDate.java
+++ src/main/java/com/codeQuartette/myTime/domain/MyDate.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -23,4 +26,34 @@ public class MyDate {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "myDate", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<HabitHasDate> habitHasDates = new ArrayList<>();
+
+    public MyDate(LocalDate date, User user) {
+        this.date = date;
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyDate myDate = (MyDate) o;
+        return date.equals(myDate.date) && user.equals(myDate.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, user);
+    }
+
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                "id=" + id +
+                ", date=" + date +
+                ", user=" + user.getNickname() +
+                '}';
+    }
 }

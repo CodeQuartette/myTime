28347497f11f0f9a5b1eab3217fcba625e7e356f
Tree <gmldbs1109@naver.com>
2023-07-20 00:15:30 +0900

    feat[#10]: 습관 생성 API 구현
    - HabitServiceImpl에서 습관 생성 시
    1. 습관 날짜에 해당하는 myDate saveAll(이미 있는 값은 제외하고 save)
    2. saveAll은 bulkRepository로 사용하고, 저장된 MyDate List는 jpa in절 사용해서 조회하고 return
    3. Habit save
    4. 2에서 리턴된 MyDate List와 Habit의 조인 테이블인 HabitHasDate saveAll(bulk insert)
    
    총 4번의 쿼리 날라감

--- src/main/java/com/codeQuartette/myTime/service/impl/HabitServiceImpl.java
+++ src/main/java/com/codeQuartette/myTime/service/impl/HabitServiceImpl.java
@@ -2,15 +2,19 @@ package com.codeQuartette.myTime.service.impl;
 
 import com.codeQuartette.myTime.controller.dto.HabitDTO;
 import com.codeQuartette.myTime.domain.Habit;
+import com.codeQuartette.myTime.domain.HabitHasDate;
 import com.codeQuartette.myTime.domain.MyDate;
 import com.codeQuartette.myTime.exception.HabitNotFoundException;
 import com.codeQuartette.myTime.repository.HabitRepository;
+import com.codeQuartette.myTime.service.HabitHasDateService;
 import com.codeQuartette.myTime.service.HabitService;
 import com.codeQuartette.myTime.service.MyDateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,15 +23,24 @@ public class HabitServiceImpl implements HabitService {
 
     private final HabitRepository habitRepository;
     private final MyDateService myDateService;
+    private final HabitHasDateService habitHasDateService;
 
     @Override
+    @Transactional
     public void create(Long userId, HabitDTO.Request habitRequestDTO) {
         Habit habit = Habit.create(habitRequestDTO);
         List<LocalDate> allHabitDates =
                 myDateService.checkAllDateByStartDateAndEndDate(habit.getStartDate(), habit.getEndDate(), habitRequestDTO.getRepeatDay());
         List<MyDate> newMyDates = myDateService.validateDates(userId, allHabitDates);
-        myDateService.saveAll(newMyDates);
-        habitRepository.save(Habit.create(habitRequestDTO));
+        List<MyDate> saveMyDates = myDateService.saveAll(newMyDates);
+
+        habit = habitRepository.save(habit);
+
+        List<HabitHasDate> habitHasDates = new ArrayList<>();
+        for(MyDate saveMyDate : saveMyDates) {
+            habitHasDates.add(HabitHasDate.builder().myDate(saveMyDate).habit(habit).build());
+        }
+        habitHasDateService.saveAll(habitHasDates);
     }
 
     @Override

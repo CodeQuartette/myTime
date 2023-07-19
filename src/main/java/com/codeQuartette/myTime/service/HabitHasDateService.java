81302dadfddef458b1f86b78aedc5f0afe1697bd
Tree <gmldbs1109@naver.com>
2023-07-20 00:06:09 +0900

    feat[#10]: 습관 생성 API 구현
    - HabitHasDate의 bulk insert를 위한 service 생성

---
+++ src/main/java/com/codeQuartette/myTime/service/HabitHasDateService.java
@@ -0,0 +1,10 @@
+package com.codeQuartette.myTime.service;
+
+import com.codeQuartette.myTime.domain.HabitHasDate;
+
+import java.util.List;
+
+public interface HabitHasDateService {
+
+    public void saveAll(List<HabitHasDate> habitHasDates);
+}

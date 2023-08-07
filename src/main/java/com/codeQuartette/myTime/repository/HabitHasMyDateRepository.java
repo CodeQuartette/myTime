bcf04d0b6084bb3a7eb4c83eaa71350a90094941
Tree <gmldbs1109@naver.com>
2023-08-07 14:51:50 +0900

    feat[#45]: 습관 월별 조회 구현

--- src/main/java/com/codeQuartette/myTime/repository/HabitHasMyDateRepository.java
+++ src/main/java/com/codeQuartette/myTime/repository/HabitHasMyDateRepository.java
@@ -5,6 +5,13 @@ import com.codeQuartette.myTime.domain.HabitHasMyDateId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface HabitHasMyDateRepository extends JpaRepository<HabitHasMyDate, HabitHasMyDateId> {
+
+    List<HabitHasMyDate> findAllByMyDate_DateIs(LocalDate date);
+
+    List<HabitHasMyDate> findAllByMyDate_DateBetween(LocalDate startDate, LocalDate endDate);
 }

368beee4a4320e6993f2bd321174a5c3918753fa
Tree <gmldbs1109@naver.com>
2023-08-18 16:09:30 +0900

    - 날짜별 습관에서 미완료 메소드 삭제 구현 - 특정 날짜와 유저에 해당하는 날짜별 습관 조회 메소드 구현 - 시작 날짜와 끝 날짜 사이에 해당하며, 유저에 해당하는 날짜별 습관 조회 메소드 구현

--- src/main/java/com/codeQuartette/myTime/service/HabitHasMyDateService.java
+++ src/main/java/com/codeQuartette/myTime/service/HabitHasMyDateService.java
@@ -1,10 +1,18 @@
 package com.codeQuartette.myTime.service;
 
 import com.codeQuartette.myTime.domain.HabitHasMyDate;
+import com.codeQuartette.myTime.domain.User;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface HabitHasMyDateService {
 
     public void saveAll(List<HabitHasMyDate> habitHasMyDates);
+
+    public void deleteAllNotDone(Long habitId);
+
+    public List<HabitHasMyDate> findAllByMyDateAndUser(User user, LocalDate date);
+
+    public List<HabitHasMyDate> findAllByUserAndStartDateAndEndDate(User user, LocalDate startDate, LocalDate endDate);
 }
--- src/main/java/com/codeQuartette/myTime/service/impl/HabitHasMyMyDateServiceImpl.java
+++ src/main/java/com/codeQuartette/myTime/service/impl/HabitHasMyMyDateServiceImpl.java
@@ -1,11 +1,14 @@
 package com.codeQuartette.myTime.service.impl;
 
 import com.codeQuartette.myTime.domain.HabitHasMyDate;
+import com.codeQuartette.myTime.domain.User;
 import com.codeQuartette.myTime.repository.HabitHasMyDateBulkRepository;
+import com.codeQuartette.myTime.repository.HabitHasMyDateRepository;
 import com.codeQuartette.myTime.service.HabitHasMyDateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -17,5 +20,17 @@ public class HabitHasMyMyDateServiceImpl implements HabitHasMyDateService {
 
     public void saveAll(List<HabitHasMyDate> habitHasMyDates) {
         habitHasDateBulkRepository.saveAll(habitHasMyDates);
+    }
+
+    public void deleteAllNotDone(Long habitId) {
+        habitHasDateBulkRepository.deleteAllNotDone(habitId);
+    }
+
+    public List<HabitHasMyDate> findAllByMyDateAndUser(User user, LocalDate date) {
+        return habitHasMyDateRepository.findAllByMyDate_UserAndMyDate_DateIs(user, date);
+    }
+
+    public List<HabitHasMyDate> findAllByUserAndStartDateAndEndDate(User user, LocalDate startDate, LocalDate endDate) {
+        return habitHasMyDateRepository.findAllByMyDate_UserAndMyDate_DateBetween(user, startDate, endDate);
     }
 }

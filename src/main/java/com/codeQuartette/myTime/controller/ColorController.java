14a3a1a164f912f2f931bfef23cae1bf719a14d1
malaheaven <yunhg95@gmail.com>
2023-10-19 16:40:32 +0900

    feat[#88]: color 응답코드 공통화 및 test 코드, docs 수정

--- src/main/java/com/codeQuartette/myTime/controller/ColorController.java
+++ src/main/java/com/codeQuartette/myTime/controller/ColorController.java
@@ -1,8 +1,9 @@
 package com.codeQuartette.myTime.controller;
 
+import com.codeQuartette.myTime.controller.globalResponse.ResponseDTO;
+import com.codeQuartette.myTime.controller.globalResponse.ResponseType;
 import com.codeQuartette.myTime.domain.value.Color;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import java.util.List;
 public class ColorController {
 
     @GetMapping("/color")
-    public ResponseEntity<List<String>> getColors() {
-        return ResponseEntity.ok(Color.getColors());
+    public ResponseDTO<List<String>> getColors() {
+        return ResponseDTO.from(ResponseType.SUCCESS,Color.getColors());
     }
 }
